fd0e640db106596e17a99b3394c3789d99fdbeda
Hannes Kanthak <hannes.kanthak@implisense.com>
2025-06-10 14:50:31 +0200

    Add clean up job in main

--- cmd/whisperbin/main.go
+++ cmd/whisperbin/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"whisperbin/internal/storage"
 	"whisperbin/internal/web"
@@ -11,6 +12,14 @@ import (
 func main() {
 	store := storage.NewStore()
 	handler := web.NewHandler(store)
+
+	go func() {
+		ticker := time.NewTicker(5 * time.Minute)
+		defer ticker.Stop()
+		for range ticker.C {
+			store.CleanupExpired()
+		}
+	}()
 
 	http.Handle("/", handler.Routes())
 	http.HandleFunc("/ws", handler.WebSocketHandler)
